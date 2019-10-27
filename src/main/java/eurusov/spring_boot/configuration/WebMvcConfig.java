f6c5acc16e05f3373eab24db4a44f0f5d7237411
eurusov <eurusov@users.noreply.github.com>
2019-10-27 15:00:32 +0300

    Got rid of login controller via addViewControllers from WebMvcConfigurer.

--- src/main/java/eurusov/spring_boot/configuration/WebMvcConfig.java
+++ src/main/java/eurusov/spring_boot/configuration/WebMvcConfig.java
@@ -3,6 +3,7 @@ package eurusov.spring_boot.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -11,5 +12,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("login");
+        registry.addViewController("/login").setViewName("login");
     }
 }
